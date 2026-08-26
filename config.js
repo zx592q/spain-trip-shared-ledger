e1cb01ef579ef9be8889335c20bf3fa09f918521
zx592q <zx592q@gmail.com>
2026-08-26 10:27:25 +0800

    Connect travel ledger to Google Sheets web app

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-// Paste the deployed Google Apps Script Web App URL between the quotes.
-window.SPREADSHEET_API_URL = '';
+window.SPREADSHEET_API_URL = 'https://script.google.com/macros/s/AKfycbzStKWMbzkU1XuBWgNXnLmW6Xo3JV8UIkgE2tlR9wIM6lHy2cVTg324w9ic5aIZVLFT/exec';
